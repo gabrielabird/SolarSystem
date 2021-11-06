cc1738112dd41caace8827532a32540f3a0c3062
gabri <bird.gs88@gmail.com>
2021-11-06 11:54:07 -0400

    featureSun added with a bug that needs to be resolved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,13 @@ public class Main {
         andromeda.Sun=1;
         andromeda.Planet=2;
         andromeda.Moon=4;
+
+        featureSun mikeSun=new featureSun();
+        mikeSun.color="blue";
+        mikeSun.heat="564684132";
+        mikeSun.temp="4F";
+        mikeSun.radius="0.009";
+        mikeSun.maritalStatus="single";
+
     }
 }
---
+++ src/com/company/featureSun.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class featureSun {
+
+    String heat;
+    String temp;
+    int radius;
+    String color;
+    String maritalStatus;
+
+}
